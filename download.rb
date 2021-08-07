f5dd54a28e8546a324dfa3adfa5675005326e8e5
Iwaco <me@iwaco.pink>
2021-08-07 15:55:55 +0900

    Update

--- download.rb
+++ download.rb
@@ -8,31 +8,46 @@ unless index
   exit 1
 end
 
-agent = Mechanize.new
-index = agent.get(index)
+def download(index)
 
-title = index.at("#info h2").inner_text.gsub(/\//, '')
-puts title
-FileUtils.mkdir(title)
+  agent = Mechanize.new
+  index = agent.get(index)
 
-input_filenames = []
-files = []
-index.search("#thumbnail-container .thumb-container a noscript img").each do |tb|
-  img = tb[:src].gsub(/t\.nhentai/, 'i.nhentai').gsub(/(\d+)t\.(jpg|png)$/, '\1.\2')
-  puts img
-  basename = File.basename(img)
-  input_filenames << basename
-  save_to = File.join(title, basename)
-  files << save_to
-  agent.get(img).save(save_to)
+  # title = index.at("#info h2").inner_text.gsub(/\//, '')
+  title = index.at("#info h2") || index.at("#info h1")
+  title = title.inner_text.gsub(/\//, '')
+
+  puts title
+  FileUtils.mkdir(title)
+
+  input_filenames = []
+  files = []
+  index.search("#thumbnail-container .thumb-container a noscript img").each do |tb|
+    img = tb[:src].gsub(/t\.nhentai/, 'i.nhentai').gsub(/(\d+)t\.(jpg|png)$/, '\1.\2')
+    puts img
+    basename = File.basename(img)
+    input_filenames << basename
+    save_to = File.join(title, basename)
+    files << save_to
+    agent.get(img).save(save_to)
+  end
+
+  Zip::File.open("#{title}.zip", Zip::File::CREATE) do |zipfile|
+    input_filenames.each_with_index do |filename, i|
+      # Two arguments:
+      # - The name of the file as it will appear in the archive
+      # - The original file, including the path to find it
+      zipfile.add(filename, files[i])
+    end
+    zipfile.get_output_stream("myFile") { |f| f.write "myFile contains just this" }
+  end
+
 end
 
-Zip::File.open("#{title}.zip", Zip::File::CREATE) do |zipfile|
-  input_filenames.each_with_index do |filename, i|
-    # Two arguments:
-    # - The name of the file as it will appear in the archive
-    # - The original file, including the path to find it
-    zipfile.add(filename, files[i])
+ARGV.each do |index|
+  if /^\d+$/ =~ index
+    index = "https://nhentai.net/g/#{index}/"
   end
-  zipfile.get_output_stream("myFile") { |f| f.write "myFile contains just this" }
+  puts "Download: #{index}"
+  download(index)
 end
